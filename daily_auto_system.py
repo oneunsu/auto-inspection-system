6656880f9723c726450f3f8097a9166d8995fd45
oneunsu <jangdmstn@naver.com>
2025-12-08 16:17:23 +0900

    Update daily_auto_system.py

--- daily_auto_system.py
+++ daily_auto_system.py
@@ -129,18 +129,9 @@ try:
 
     # 300: 점검
     try:
-        try:
-            general_check = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id="check_tblList"]/tbody/tr[1]/th[2]/input")))
-        except TimeoutException as e:
-            fail(301, "일반 체크박스 요소 찾기 실패", e, driver)
-        try:
-            electrical_check = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id="check_tblList"]/tbody/tr[8]/th[2]/input")))
-        except TimeoutException as e:
-            fail(302, "전기 체크박스 요소 찾기 실패", e, driver)
-        try:
-            fire_check = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id="check_tblList"]/tbody/tr[13]/th[2]/input")))
-        except TimeoutException as e:
-            fail(303, "전기 체크박스 요소 찾기 실패", e, driver)
+        general_check = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id="check_tblList"]/tbody/tr[1]/th[2]/input")))
+        electrical_check = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id="check_tblList"]/tbody/tr[8]/th[2]/input")))
+        fire_check = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id="check_tblList"]/tbody/tr[13]/th[2]/input")))
         mark(300, "체크박스 찾기")
     except TimeoutException as e:
         fail(304, "체크박스 요소 찾기 실패", e, driver)
@@ -170,6 +161,7 @@ finally:
         driver.quit()
     except Exception:
         pass
+
 
 
 
